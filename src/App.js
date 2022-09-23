6a925933c8a9d4563c556a052084a49ad6f50e32
HETRA04 <abdulla.arteh@gmail.com>
2022-09-23 22:22:54 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+/*import React from 'react';
+import Chatroom from './Components/Chatroom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import './App.css'*/
+
+
+import React from "react";
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <p>hello</p>
+     </div>
+  )
 }
 
 export default App;

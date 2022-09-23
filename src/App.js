73c4d3ba34eea9f1350dd24959660ff7cc670fdd
HETRA04 <abdulla.arteh@gmail.com>
2022-09-23 22:56:30 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import 'firebase/compat/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import './App.css'*/
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCxFEcwvyw71RtSAeh3GI-YTZyUcS05T44",
+  authDomain: "chatapp-9ea55.firebaseapp.com",
+  projectId: "chatapp-9ea55",
+  storageBucket: "chatapp-9ea55.appspot.com",
+  messagingSenderId: "932648553494",
+  appId: "1:932648553494:web:039818cafe1b2834f17937",
+  measurementId: "G-J4RXV6KC7J"
+})
 
 import React from "react";
 
@@ -15,7 +24,7 @@ function App() {
   
   return (
     <div className="App">
-      <p>hello</p>
+      
      </div>
   )
 }
